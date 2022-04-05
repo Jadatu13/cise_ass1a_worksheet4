700f32a2cf05d726308843903425fc62c9d291d1
Jacob Tupe <jadatu13@outlook.com>
2022-04-05 17:43:16 +1200

    Added information for SE-Practice.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article";
+import NotFoundPage from "./pages/404";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+const App = () => {
   return (
-    <div>
-      <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-      <ul className="header">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/SEPractice">Select the Practice</a>
-        </li>
-        <li>
-          <a href="/SubmitArticle">Submit an Article</a>
-        </li>
-      </ul>
-      <div className="content"></div>
-    </div>
+    <Router>
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li>
+            <a href="/pages/Home.js">Home</a>
+          </li>
+          <li>
+            <a href="/SEPractice">Select the Practice</a>
+          </li>
+          <li>
+            <a href="/SubmitArticle">Submit an Article</a>
+          </li>
+        </ul>
+        <div className="content"></div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
